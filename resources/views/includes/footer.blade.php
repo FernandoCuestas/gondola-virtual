7fe21e2e950cd09d8fad0a2eb7428c9a54e30d86
unknown <fernandocuestas886@gmail.com>
2020-01-22 00:09:20 -0300

    first commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="pull-left">
+            
+        </nav>
+        <div class="copyright pull-right">
+            &copy; 2017.
+        </div>
+    </div>
+</footer>
